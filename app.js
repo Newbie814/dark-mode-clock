05a946a2a1aff08ea2c18338d713c3a27905bb38
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-08-16 11:26:55 -0400

    dark mode fucntionality

--- app.js
+++ app.js
@@ -68,6 +68,17 @@ function setTime() {
   dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
 }
 
+toggle.addEventListener('click', (e) => {
+  const html = document.querySelector('html');
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark');
+    e.target.innerHTML = 'Dark mode';
+  } else {
+    html.classList.add('dark');
+    e.target.innerHTML = 'Light mode';
+  }
+});
+
 // StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
